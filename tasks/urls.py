9aec60a58ad842613cb2546f90e2afc9997e6dad
Your Name <you@example.com>
2020-10-19 20:51:01 +0300

    Add deadline of task

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('update_project/<str:pk>' , views.updateProject, name="update_project"),
     path('delete_project/<str:pk>' , views.deleteProject, name="delete_project"),
     path('create_task/<str:pk>' , views.createTask, name="create_task"),
+
+    path('deadline_task/<str:pk>' , views.deadlineTask, name="deadline_task"),
+
     path('update_task/<str:pk>' , views.updateTask, name="update_task"),
     path('delete_task/<str:pk>' , views.deleteTask, name="delete_task"),
    
